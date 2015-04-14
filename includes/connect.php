475cb095c45517834a2b36abcca66df03c82498b
Andrew Olivas <andrewrolivas@gmail.com>
2015-04-14 16:28:18 -0700

    more comments so i can get that fresh A

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-
+// gets your username,password and the task column from localhost
 $mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
 if ($mysqli->maxdb_connect_error) {
 	// $mysqli->connect_error then we want it to die and have this message
@@ -8,6 +8,7 @@ if ($mysqli->maxdb_connect_error) {
 	
 }
 else{
+	// when a connection is made this echo pops up
 	echo "Conection made";
 }
 $mysqli->close();
