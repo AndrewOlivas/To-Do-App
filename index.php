1672bec24afbfaf35f4e0a06acde125e59f0f1ce
Andrew Olivas <andrewrolivas@gmail.com>
2015-04-14 16:18:26 -0700

    video 5 and 6 complete.

--- index.php
+++ index.php
@@ -7,7 +7,13 @@
 </head>
 <body>
 	<div class="wrap">
-	
+		<div class="task-list">
+			<ul>
+				<?php require["includes/connect.php"] ?>
+			</ul>
+		</div>
 	</div>
+	<form class="add-new-task" autocomplete="off">
+	<input type="text" none="new-task" placeholder="Add new item...">
 </body>
 </html>
