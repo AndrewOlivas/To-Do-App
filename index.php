3e3c8384287971b9a8527da7e2f6abfcf4951b42
Andrew Olivas <andrewrolivas@gmail.com>
2015-04-15 09:12:16 -0700

    milestone 8 and some of milestone 9

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<title>
 		TO-Do Lists
 	</title>
+	<!-- links main css to this php/html file -->
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
@@ -10,13 +11,20 @@
 		<div class="task-list">
 			<ul>
 			<!-- this connects the file 'connect.php' from the 'includes' folder -->
-				<?php require["includes/connect.php"] ?> 
+				<?php require("includes/connect.php"); ?> 
 			</ul>
 		</div>
 	</div>
 	<!-- new form for when you add a new task  -->
-	<form class="add-new-task" autocomplete="off">
+		<form class="add-new-task" autocomplete="off">
 	<!-- input new item to your task -->
-	<input type="text" none="new-task" placeholder="Add new item...">
+			<input type="text" none="new-task" placeholder="Add new item...">
+		</form>
 </body>
+<script src="hhtp://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	add_task(); //calls the add task function
+
+</script>
+
 </html>
