85f90f50b399a39b514123d5eea532c46b6dfb4f
Andrew Olivas <andrewrolivas@gmail.com>
2015-06-01 10:30:21 -0700

    added includes, add task delete task, and connect.

--- includes/connect.php
+++ includes/connect.php
@@ -1,18 +1,13 @@
 <?php
-// gets your username,password and the task column from localhost
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
-if ($mysqli->maxdb_connect_error) {
-	// $mysqli->connect_error then we want it to die and have this message
-	die('connect error('. $mysqli>connect_errno .')'
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
+if ($mysqli->connect_error) {
+	die('Connect Error('. $mysqli>connect_errno .')'
 		.$mysqli->connect_error);
-	
 }
 else{
-	// when a connection is made this echo pops up
-	echo "Conection made";
+	echo "Connection made";
 }
 $mysqli->close();
-
 
 
 ?>
